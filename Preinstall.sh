ce3554761a192e73bf0257ab3dc989ad4fdeea97
dspates81 <72100421+dspates81@users.noreply.github.com>
2020-09-30 21:54:38 -0400

    Update Preinstall.sh

--- Preinstall.sh
+++ Preinstall.sh
@@ -33,7 +33,8 @@ echo "--------------------------------------"
 
 # disk prep
 sgdisk -Z ${DISK} # zap all on disk
-sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
+sgdisk -a ${DISK} # new gpt disk 2048 alignment
+sgdisk -a ${DISK} # new gpt disk 2048 alignment
 
 # create partitions
 sgdisk -n 1:0:+5120M ${DISK} # partition 1 (UEFI SYS), default start block, 512MB
@@ -49,65 +50,3 @@ sgdisk -t 2:8300 ${DISK}
 sgdisk -c 1:"UEFISYS" ${DISK}
 sgdisk -c 2:"ROOT" ${DISK}
 sgdisk -c 2:"FREESPACE" ${DISK}
-
-# make filesystems
-echo -e "\nCreating Filesystems...\n$HR"
-
-mkfs.vfat -F32 -n "UEFISYS" "${DISK}1"
-mkfs.ext4 -L "ROOT" "${DISK}2"
-mkfs.ext4 -L "linuxfilesystem" "${DISK}2"
-
-# mount target
-
-mount -t ext4 "${DISK}2" /mnt
-mkdir /mnt/home
-mount -t vfat "${DISK}3" /mnt/home/
-mkdir /mnt/etc
-
-echo "--------------------------------------"
-echo "-- Arch Install on Main Drive       --"
-echo "--------------------------------------"
-
-genfstab -U -p /mnt >> /mnt/etc/fstab
-pacstrap -i /mnt base
-arch-chroot /mnt
-pacman -S linux linux-headers
-pacman -S nano
-
-echo "--------------------------------------"
-echo "-- Bootloader Systemd Installation  --"
-echo "--------------------------------------"
-
-mkdir /mnt/boot/efi
-mount -t vfat "${DISK}1" /mnt/boot/
-bootctl --path=/ boot install
-
-cat <<EOF > /boot/loader/entries/arch.conf
-
-title Arch Linux  
-linux /vmlinuz-linux  
-initrd  /initramfs-linux.img  
-options root=${DISK}1 rw
-EOF
-
-echo "--------------------------------------"
-echo "--          Network Setup           --"
-echo "--------------------------------------"
-pacman -S networkmanager wpa_supplicant wireless_tools netctl dialog dhclient --noconfirm --needed
-systemctl enable --now NetworkManager
-pacman -S base-devel openssh
-systemctl enable ssh
-
-
-echo "--------------------------------------"
-echo "--      Set Password for Root       --"
-echo "--------------------------------------"
-echo "Enter password for root user: "
-passwd root
-
-exit
-umount -R /mnt
-
-echo "--------------------------------------"
-echo "--   SYSTEM READY FOR FIRST BOOT    --"
-echo "--------------------------------------"
