c85d72d119973df9c89e8d23f19a29c44d56130c
dspates81 <72100421+dspates81@users.noreply.github.com>
2020-10-01 22:04:49 -0400

    Update Arch_Installation.sh

--- Arch_Installation.sh
+++ Arch_Installation.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 
 echo "--------------------------------------"
 echo "-- Arch Install on Main Drive       --"
@@ -7,16 +7,23 @@ echo "--------------------------------------"
 genfstab -U -p /mnt >> /mnt/etc/fstab
 pacstrap -i /mnt base
 arch-chroot /mnt
+mkdir /mnt/boot/efi
+mount  "${DISK}1" /mnt/boot/EFI
 pacman -S nano
 pacman -S linux linux-headers
 mkinitcpio -p linux
+
+pacman -S grub efibootmgr dosfstools os-prober mtools
+grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+grub-mkconfig -o /boot/grub/grub.cfg
 
 
 echo "--------------------------------------"
 echo "--          Network Setup           --"
 echo "--------------------------------------"
-pacman -S networkmanager wpa_supplicant wireless_tools netctl dialog dhclient
-systemctl enable now NetworkManager
+pacman -S networkmanager wpa_supplicant wireless_tools netctl dialog dhclient 
+systemctl enable NetworkManager
 pacman -S base-devel openssh
 systemctl enable ssh
 
