38a039a6b3b280a8bde58c4c7c5b2d32075bd52f
dspates81 <72100421+dspates81@users.noreply.github.com>
2020-10-02 11:57:34 -0400

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -63,49 +63,21 @@ echo "--------------------------------------"
 echo "-- Arch Install on Main Drive       --"
 echo "--------------------------------------"
 
-pacstrap /mnt base 
-pacman -S base-devel linux-firmware 
+ 
+ 
 pacman -S vim nano sudo 
 genfstab -U /mnt >> /mnt/etc/fstab
-#pacstrap -i /mnt base
+
 arch-chroot /mnt
-fallocate -l 2GB /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-ln -sf /usr/share/zoneinfo/America/New York /etc/localtime
-hwclock --systohc
-
-
-pacman -S os-prober mtools do sfstools
-pacman -S efibootmgr
-
-pacman -S networkmanager network-manager-applet 
-pacman -S wpa_supplicant wireless_tools netctl dialog dhclient
-systemctl enable now NetworkManager
-pacman -S nano
-systemctl enable ssh
-
-mkinitcpio -p linux
-pacman -S intel-ucode
-Pacman -S xorg-server
-pacman -S mesa
-pacman -S nvidia nvidia-utils
+pacman -S base-devel linux-firmware
+bootctl --path=/boot install
+pacstrap -i /mnt base
 
 echo "--------------------------------------"
 echo "--      Set Password for Root       --"
 echo "--------------------------------------"
 echo "Enter password for root user: "
 passwd root
-
-echo "--------------------------------------"
-echo "-- Bootloader Systemd Installation  --"
-echo "--------------------------------------"
-
-
-
-bootctl --path=/boot install
-
 
 
 echo "--------------------------------------"
