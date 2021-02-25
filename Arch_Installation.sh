c4a0c4821ac9a76512f7d7c461b0e7e5598e00ac
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-02-25 18:06:16 -0500

    Update Arch_Installation.sh

--- Arch_Installation.sh
+++ Arch_Installation.sh
@@ -7,9 +7,9 @@ echo "--------------------------------------"
 genfstab -U -p /mnt >> /mnt/etc/fstab
 pacstrap -i /mnt base
 arch-chroot /mnt
-mkdir /mnt/boot/efi
+mkdir -p /mnt/boot/efi
 mount  "${DISK}1" /mnt/boot/EFI
-mount  "${DISK}1" /mnt
+mount  "${DISK}2" /mnt
 pacman -S linux linux-headers nano sudo man 
 mkinitcpio -p linux
 
@@ -34,6 +34,58 @@ echo "--------------------------------------"
 echo "--          Network Setup           --"
 echo "--------------------------------------"
 pacman -S networkmanager wpa_supplicant wireless_tools netctl dialog dhclient base-devel openssh
+systemctl enable NetworkManager
+systemctl enable ssh
+
+
+echo "--------------------------------------"
+echo "--      Set Password for Root       --"
+echo "--------------------------------------"
+echo "Enter password for root user: "
+passwd root
+
+umount -R /mnt
+
+echo "--------------------------------------"
+echo "--   SYSTEM READY FOR FIRST BOOT    --"
+echo "--------------------------------------"
+!/usr/bin/env bash
+
+echo "--------------------------------------"
+echo "-- Arch Install on Main Drive       --"
+echo "--------------------------------------"
+
+genfstab -U -p /mnt >> /mnt/etc/fstab
+pacstrap -i /mnt base
+arch-chroot /mnt
+mkdir /mnt/boot/efi
+mount  "${DISK}1" /mnt/boot/EFI
+mount  "${DISK}2" /mnt
+pacman -S linux linux-headers nano sudo man 
+mkinitcpio -p linux
+
+
+
+dd if=/dev/zero of=swapfile bs=1M count=5120 status=progress
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+
+echo "
+/swapfile		none	swap	defaults	0 0
+" >> /etc/fstab
+
+pacman -S grub efibootmgr dosfstools os-prober mtools
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck
+cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+grub-mkconfig -o /boot/grub/grub.cfg
+
+
+
+echo "--------------------------------------"
+echo "--          Network Setup           --"
+echo "--------------------------------------"
+pacman -S networkmanager network-manager-applet wpa_supplicant wireless_tools mtools netctl dialog dhclient multilib-devel base-devel openssh
 systemctl enable NetworkManager
 systemctl enable ssh
 
