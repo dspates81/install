a4da45456e93d59f54c4aa532d54f1f4f0211740
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-02-25 18:53:29 -0500

    Update Arch_Installation.sh

--- Arch_Installation.sh
+++ Arch_Installation.sh
@@ -1,9 +1,10 @@
 !/usr/bin/env bash
-
+sleep 4s
 echo "--------------------------------------"
 echo "-- Arch Install on Main Drive       --"
 echo "--------------------------------------"
 
+sleep 4s
 genfstab -U -p /mnt >> /mnt/etc/fstab
 pacstrap -i /mnt base
 arch-chroot /mnt
@@ -14,58 +15,7 @@ pacman -S linux linux-headers nano sudo man
 mkinitcpio -p linux
 
 
-
-dd if=/dev/zero of=swapfile bs=1M count=5120 status=progress
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-
-echo "
-/swapfile		none	swap	defaults	0 0
-" >> /etc/fstab
-
-pacman -S grub efibootmgr dosfstools os-prober mtools
-grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
-cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
-grub-mkconfig -o /boot/grub/grub.cfg
-
-
-echo "--------------------------------------"
-echo "--          Network Setup           --"
-echo "--------------------------------------"
-pacman -S networkmanager wpa_supplicant wireless_tools netctl dialog dhclient base-devel openssh
-systemctl enable NetworkManager
-systemctl enable ssh
-
-
-echo "--------------------------------------"
-echo "--      Set Password for Root       --"
-echo "--------------------------------------"
-echo "Enter password for root user: "
-passwd root
-
-umount -R /mnt
-
-echo "--------------------------------------"
-echo "--   SYSTEM READY FOR FIRST BOOT    --"
-echo "--------------------------------------"
-!/usr/bin/env bash
-
-echo "--------------------------------------"
-echo "-- Arch Install on Main Drive       --"
-echo "--------------------------------------"
-
-genfstab -U -p /mnt >> /mnt/etc/fstab
-pacstrap -i /mnt base
-arch-chroot /mnt
-mkdir /mnt/boot/efi
-mount  "${DISK}1" /mnt/boot/EFI
-mount  "${DISK}2" /mnt
-pacman -S linux linux-headers nano sudo man 
-mkinitcpio -p linux
-
-
-
+sleep 4s
 dd if=/dev/zero of=swapfile bs=1M count=5120 status=progress
 chmod 600 /swapfile
 mkswap /swapfile
@@ -77,11 +27,10 @@ echo "
 
 pacman -S grub efibootmgr dosfstools os-prober mtools
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck
-cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
 grub-mkconfig -o /boot/grub/grub.cfg
 
 
-
+sleep 4s
 echo "--------------------------------------"
 echo "--          Network Setup           --"
 echo "--------------------------------------"
@@ -89,7 +38,7 @@ pacman -S networkmanager network-manager-applet wpa_supplicant wireless_tools mt
 systemctl enable NetworkManager
 systemctl enable ssh
 
-
+sleep 4s
 echo "--------------------------------------"
 echo "--      Set Password for Root       --"
 echo "--------------------------------------"
