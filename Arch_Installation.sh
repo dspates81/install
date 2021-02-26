c0e228abd0a074153ef949b8d44f3d37afd57d91
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-02-25 20:59:27 -0500

    Update Arch_Installation.sh

--- Arch_Installation.sh
+++ Arch_Installation.sh
@@ -1,48 +1,52 @@
+!/usr/bin/env bash
 
-#!/usr/bin/env bash
-#-------------------------------------------------------------------------
-#      _          _    __  __      _   _
-#     /_\  _ _ __| |_ |  \/  |__ _| |_(_)__
-#    / _ \| '_/ _| ' \| |\/| / _` |  _| / _|
-#   /_/ \_\_| \__|_||_|_|  |_\__,_|\__|_\__|
-#  Arch Linux Post Install Setup and Config
-#-------------------------------------------------------------------------
-
-if ! source install.conf; then
-	read -p "Please enter hostname:" hostname
-
-	read -p "Please enter username:" username
-
-	read -sp "Please enter password:" password
-
-	read -sp "Please repeat password:" password2
-
-	# Check both passwords match
-	if [ "$password" != "$password2" ]; then
-	    echo "Passwords do not match"
-	    exit 1
-	fi
-  printf "hostname="$hostname"\n" >> "install.conf"
-  printf "username="$username"\n" >> "install.conf"
-  printf "password="$password"\n" >> "install.conf"
-fi
+echo "--------------------------------------"
+echo "-- Arch Install on Main Drive       --"
+echo "--------------------------------------"
 
 
-echo "-------------------------------------------------"
-echo "       Setup Language to US and set locale       "
-echo "-------------------------------------------------"
+genfstab -U -p /mnt >> /mnt/etc/fstab
+pacstrap -i /mnt base
+arch-chroot /mnt
+mkdir -p /mnt/boot/efi
+mount  "${DISK}1" /mnt/boot/EFI
+mount  "${DISK}2" /mnt
+pacman -S linux linux-headers nano sudo man 
+mkinitcpio -p linux
 
-ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
-hwclock --systohc
-nano /etc/locale.gen
-echo "LANG=en_UTF-8" > etc/locale.conf
-local-gen
 
-# Set keymaps
-localectl --no-ask-password set-keymap us
 
-# Hostname
-hostnamectl --no-ask-password set-hostname $hostname
+dd if=/dev/zero of=swapfile bs=1M count=5120 status=progress
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
 
-# Add sudo no password rights
-sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+echo "
+/swapfile		none	swap	defaults	0 0
+" >> /etc/fstab
+
+pacman -S grub efibootmgr dosfstools os-prober mtools
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+
+
+
+echo "--------------------------------------"
+echo "--          Network Setup           --"
+echo "--------------------------------------"
+pacman -S networkmanager network-manager-applet wpa_supplicant wireless_tools mtools netctl dialog dhclient multilib-devel base-devel openssh
+systemctl enable NetworkManager
+systemctl enable ssh
+
+
+echo "--------------------------------------"
+echo "--      Set Password for Root       --"
+echo "--------------------------------------"
+echo "Enter password for root user: "
+passwd root
+
+umount -R /mnt
+
+echo "--------------------------------------"
+echo "--   SYSTEM READY FOR FIRST BOOT    --"
+echo "--------------------------------------"
