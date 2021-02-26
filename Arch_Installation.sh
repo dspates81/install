379c0847bcd27efafa82031814689db67f6ee25d
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-02-25 20:09:46 -0500

    Update Arch_Installation.sh

--- Arch_Installation.sh
+++ Arch_Installation.sh
@@ -1,52 +1,48 @@
-!/usr/bin/env bash
-sleep 4s
-echo "--------------------------------------"
-echo "-- Arch Install on Main Drive       --"
-echo "--------------------------------------"
 
-sleep 4s
-genfstab -U -p /mnt >> /mnt/etc/fstab
-pacstrap -i /mnt base
-arch-chroot /mnt
-mkdir -p /mnt/boot/efi
-mount  "${DISK}1" /mnt/boot/EFI
-mount  "${DISK}2" /mnt
-pacman -S linux linux-headers nano sudo man 
-mkinitcpio -p linux
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------
+#      _          _    __  __      _   _
+#     /_\  _ _ __| |_ |  \/  |__ _| |_(_)__
+#    / _ \| '_/ _| ' \| |\/| / _` |  _| / _|
+#   /_/ \_\_| \__|_||_|_|  |_\__,_|\__|_\__|
+#  Arch Linux Post Install Setup and Config
+#-------------------------------------------------------------------------
 
+if ! source install.conf; then
+	read -p "Please enter hostname:" hostname
 
-sleep 4s
-dd if=/dev/zero of=swapfile bs=1M count=5120 status=progress
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
+	read -p "Please enter username:" username
 
-echo "
-/swapfile		none	swap	defaults	0 0
-" >> /etc/fstab
+	read -sp "Please enter password:" password
 
-pacman -S grub efibootmgr dosfstools os-prober mtools
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck
-grub-mkconfig -o /boot/grub/grub.cfg
+	read -sp "Please repeat password:" password2
+
+	# Check both passwords match
+	if [ "$password" != "$password2" ]; then
+	    echo "Passwords do not match"
+	    exit 1
+	fi
+  printf "hostname="$hostname"\n" >> "install.conf"
+  printf "username="$username"\n" >> "install.conf"
+  printf "password="$password"\n" >> "install.conf"
+fi
 
 
-sleep 4s
-echo "--------------------------------------"
-echo "--          Network Setup           --"
-echo "--------------------------------------"
-pacman -S networkmanager network-manager-applet wpa_supplicant wireless_tools mtools netctl dialog dhclient multilib-devel base-devel openssh
-systemctl enable NetworkManager
-systemctl enable ssh
+echo "-------------------------------------------------"
+echo "       Setup Language to US and set locale       "
+echo "-------------------------------------------------"
 
-sleep 4s
-echo "--------------------------------------"
-echo "--      Set Password for Root       --"
-echo "--------------------------------------"
-echo "Enter password for root user: "
-passwd root
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+hwclock --systohc
+nano /etc/locale.gen
+echo "LANG=en_UTF-8" > etc/locale.conf
+local-gen
 
-umount -R /mnt
+# Set keymaps
+localectl --no-ask-password set-keymap us
 
-echo "--------------------------------------"
-echo "--   SYSTEM READY FOR FIRST BOOT    --"
-echo "--------------------------------------"
+# Hostname
+hostnamectl --no-ask-password set-hostname $hostname
+
+# Add sudo no password rights
+sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
