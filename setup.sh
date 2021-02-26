45a79b9f150e3fe28c3c0786adda7bf9fac197e4
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-02-25 21:48:24 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -31,11 +31,12 @@ echo "-------------------------------------------------"
 echo "       Setup Language to US and set locale       "
 echo "-------------------------------------------------"
 
-cp localtime > /etc/localtime
-hwclock --systohc
+
+cp localtime  /etc/localtime
+echo hwclock --systohc
 cp locale.gen > /etc/locale.gen
-cp locale.conf > etc/locale.conf
-local-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo locale-gen
 
 # Set keymaps
 localectl --no-ask-password set-keymap us
@@ -45,4 +46,102 @@ hostnamectl --no-ask-password set-hostname $hostname
 
 # Add sudo no password rights
 sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
-./formatt.sh
+
+
+
+echo "-------------------------------------------------"
+echo "-------------------Formatt----------------------"
+echo "-------------------------------------------------"
+timedatectl set-ntp true
+pacman -S --noconfirm pacman-contrib
+
+
+echo -e "\nInstalling prereqs...\n$HR"
+pacman -S --noconfirm gptfdisk btrfs-progs
+
+echo "-------------------------------------------------"
+echo "-------select your disk to format----------------"
+echo "-------------------------------------------------"
+lsblk
+
+echo "Please enter disk: (example /dev/sda)"
+read DISK
+echo "--------------------------------------"
+echo -e "\nFormatting disk...\n$HR"
+echo "--------------------------------------"
+
+# disk prep
+sgdisk -Z ${DISK} # zap all on disk
+sgdisk -a ${DISK} # new gpt disk 2048 alignment
+
+
+# create partitions
+sgdisk -n 1:0:+3000M ${DISK} # partition 1 (UEFI SYS), default start block, 3GB
+sgdisk -n 2:0:+50000M ${DISK} # partition 2 (Root), default start block, 50GB
+
+
+# set partition types
+sgdisk -t 1:ef00 ${DISK}
+sgdisk -t 2:8300 ${DISK}
+
+# label partitions
+sgdisk -c 1:"UEFISYS" ${DISK}
+sgdisk -c 2:"ROOT" ${DISK}
+
+
+# make filesystems
+echo -e "\nCreating Filesystems...\n$HR"
+
+mkfs.vfat -F32 -n "UEFISYS" "${DISK}1"
+mkfs.ext4 -L "ROOT" "${DISK}2"
+
+# mount target
+
+mount -t ext4 "${DISK}2" /mnt
+mkdir -p /dev/"${DISK}1" /mnt/boot/efi
+
+
+
+echo "--------------------------------------"
+echo "-- Arch Install on Main Drive       --"
+echo "--------------------------------------"
+
+
+genfstab -U -p /mnt >> /mnt/etc/fstab
+pacstrap -i /mnt base linux linux-headers nano sudo man 
+arch-chroot /mnt
+
+
+dd if=/dev/zero of=swapfile bs=1M count=5120 status=progress
+chmod 600 /swapfile
+echo mkswap /swapfile
+echo swapon /swapfile
+
+echo "
+/swapfile		none	swap	defaults	0 0
+" >> /etc/fstab
+
+pacman -S grub efibootmgr dosfstools os-prober mtools
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+
+
+echo "--------------------------------------"
+echo "--          Network Setup           --"
+echo "--------------------------------------"
+pacman -S networkmanager network-manager-applet wpa_supplicant wireless_tools mtools netctl dialog dhclient multilib-devel base-devel openssh
+systemctl enable NetworkManager
+systemctl enable ssh
+
+
+echo "--------------------------------------"
+echo "--      Set Password for Root       --"
+echo "--------------------------------------"
+echo "Enter password for root user: "
+passwd root
+
+umount -R /mnt
+
+echo "--------------------------------------"
+echo "--   SYSTEM READY FOR FIRST BOOT    --"
+echo "--------------------------------------"
