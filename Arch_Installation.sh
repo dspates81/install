0c4ec1cbe49dc39b99e93e6638c0fc8a46892f57
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-04 11:56:03 -0400

    Update Arch_Installation.sh

--- Arch_Installation.sh
+++ Arch_Installation.sh
@@ -1,5 +1,64 @@
 !#/bin/bash
 
+
+#-------------------------------------------------------------------------
+#      _          _    __  __      _   _
+#     /_\  _ _ __| |_ |  \/  |__ _| |_(_)__
+#    / _ \| '_/ _| ' \| |\/| / _` |  _| / _|
+#   /_/ \_\_| \__|_||_|_|  |_\__,_|\__|_\__|
+#  Arch Linux Post Install Setup and Config
+#-------------------------------------------------------------------------
+
+if ! source install.conf; then
+	read -p "Please enter hostname:" hostname
+
+	read -p "Please enter username:" username
+
+	read -sp "Please enter password:" password
+
+	read -sp "Please repeat password:" password2
+
+	# Check both passwords match
+	if [ "$password" != "$password2" ]; then
+	    echo "Passwords do not match"
+	    exit 1
+	fi
+  printf "hostname="$hostname"\n" >> "install.conf"
+  printf "username="$username"\n" >> "install.conf"
+  printf "password="$password"\n" >> "install.conf"
+fi
+
+echo "-------------------------------------------------"
+echo "Setting up mirrors for optimal download - US Only"
+echo "-------------------------------------------------"
+pacman -S --noconfirm pacman-contrib curl
+mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
+
+nc=$(grep -c ^processor /proc/cpuinfo)
+echo "You have " $nc" cores."
+echo "-------------------------------------------------"
+echo "Changing the makeflags for "$nc" cores."
+sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$nc"/g' /etc/makepkg.conf
+echo "Changing the compression settings for "$nc" cores."
+sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $nc -z -)/g' /etc/makepkg.conf
+
+echo "-------------------------------------------------"
+echo "       Setup Language to US and set locale       "
+echo "-------------------------------------------------"
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+timedatectl --no-ask-password set-timezone America/New_York
+timedatectl --no-ask-password set-ntp 1
+localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_COLLATE="" LC_TIME="en_US.UTF-8"
+
+# Set keymaps
+localectl --no-ask-password set-keymap us
+
+# Hostname
+hostnamectl --no-ask-password set-hostname $hostname
+
+
 echo "--------------------------------------"
 echo "-- Arch Install on Main Drive       --"
 echo "--------------------------------------"
@@ -15,16 +74,14 @@ echo "
 " >> /etc/fstab
 
 
-ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
-hwclock --systohc
-sed -i '177s/.//' /etc/locale.gen
-locale-gen
-#echo "KEYMAP=us" >> /etc/vconsole.conf
-echo "Dspates81" >> /etc/hostname
+#ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+#hwclock --systohc
+#sed -i '177s/.//' /etc/locale.gen
+#locale-gen
+
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 Dspates81.localdomain Dspates81" >> /etc/hosts
-
 
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
@@ -51,5 +108,9 @@ systemctl enable firewalld
 systemctl enable acpid
 
 
+# Add sudo no password rights
+sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
 
-printf "\e[1;32mDone! .\e[0m"
+
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+
