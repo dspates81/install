d05337c0ff62a2e386e4bd1b452da6ec65bc4ba6
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-06 19:34:03 -0400

    Update EFIgrubfile.sh

--- EFIgrubfile.sh
+++ EFIgrubfile.sh
@@ -8,16 +8,3 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 mv /ArchInst/mkinitcpio.conf /etc/
 mkinitcpio -p linux
-
-
-systemctl enable NetworkManager
-systemctl enable bluetooth
-#systemctl enable cups.service
-systemctl enable sshd
-systemctl enable avahi-daemon
-#systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
-systemctl enable reflector.timer
-systemctl enable fstrim.timer
-systemctl enable libvirtd
-systemctl enable firewalld
-systemctl enable acpid
