155c3a91783429b52fa930671051d41323282b62
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-06 16:56:35 -0400

    Update Arch_Installation.sh

--- Arch_Installation.sh
+++ Arch_Installation.sh
@@ -58,17 +58,12 @@ echo "
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S --needed grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pulseaudio pulseaudio-bluetooth pavucontrol pulseaudio-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+pacman -S --noconfirm --needed grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pulseaudio pulseaudio-bluetooth pavucontrol pulseaudio-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 # cups
 # pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-mv /ArchInst/grub /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
 
-mv /ArchInst/mkinitcpio.conf /etc/
-Mkinitcpio -p linux
 
 systemctl enable NetworkManager
 systemctl enable bluetooth
@@ -82,6 +77,12 @@ systemctl enable libvirtd
 systemctl enable firewalld
 systemctl enable acpid
 
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+mv /ArchInst/grub /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
+mv /ArchInst/mkinitcpio.conf /etc/
+Mkinitcpio -p linux
 
 # Add sudo no password rights
 sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
