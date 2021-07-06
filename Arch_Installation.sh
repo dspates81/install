573523522ab33e98b5939f1766f984202db47f58
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-06 16:12:31 -0400

    Update Arch_Installation.sh

--- Arch_Installation.sh
+++ Arch_Installation.sh
@@ -13,7 +13,7 @@
 echo "-------------------------------------------------"
 echo "Setting up mirrors for optimal download - US Only"
 echo "-------------------------------------------------"
-pacman -S --noconfirm --needed pacman-contrib curl
+#pacman -S --noconfirm --needed pacman-contrib curl
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 curl -s "https://archlinux.org/mirrorlist/?country=US&protocol=http&protocol=https&ip_version=4" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 
