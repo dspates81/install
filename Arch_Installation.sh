0a6bfe4831c0dded6f7561a699f18dea701d057e
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-06 16:42:12 -0400

    Update Arch_Installation.sh

--- Arch_Installation.sh
+++ Arch_Installation.sh
@@ -13,7 +13,7 @@
 echo "-------------------------------------------------"
 echo "Setting up mirrors for optimal download - US Only"
 echo "-------------------------------------------------"
-#pacman -S --noconfirm --needed pacman-contrib curl
+pacman -S --noconfirm pacman-contrib curl
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 curl -s "https://archlinux.org/mirrorlist/?country=US&protocol=http&protocol=https&ip_version=4" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 
@@ -53,37 +53,38 @@ echo "
 /swapfile		none	swap	defaults	0 0
 " >> /etc/fstab
 
-pacman -S --needed grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers wireless_tools intel-ucode xdg-utils xdg-user-dirs bash-completion openssh rsync cronie ipset iptables iptables-nft bluez bluez-utils terminus-font tlp reflector openbsd-netcat os-prober
+
 
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-
-
+pacman -S --needed grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pulseaudio pulseaudio-bluetooth pavucontrol pulseaudio-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 # cups
 # pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
-
-
-systemctl enable NetworkManager
-
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 mv /ArchInst/grub /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 mv /ArchInst/mkinitcpio.conf /etc/
-mkinitcpio -p linux
+Mkinitcpio -p linux
 
-sleep 5
+systemctl enable NetworkManager
+systemctl enable bluetooth
+#systemctl enable cups.service
+systemctl enable sshd
+systemctl enable avahi-daemon
+#systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
+systemctl enable libvirtd
+systemctl enable firewalld
+systemctl enable acpid
+
 
 # Add sudo no password rights
 sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
 
 
-
-
-
-
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
-
