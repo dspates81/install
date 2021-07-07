f39959fa8d16bc48c760ab1fbfe24fd525b3d6c3
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-06 20:53:13 -0400

    Update EFIgrubfile.sh

--- EFIgrubfile.sh
+++ EFIgrubfile.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-pacman -S --needed grub efibootmgr
+#pacman -S --needed grub efibootmgr
+#grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+#mv /ArchInst/grub /etc/default/grub
+#grub-mkconfig -o /boot/grub/grub.cfg
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-cp /ArchInst/grub /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
+bootctl --path=/boot install
+cp loader.conf /boot/loader
+cp arch.conf /boot/loader/entries
 
-cp /ArchInst/mkinitcpio.conf /etc/
+
+
+mv /ArchInst/mkinitcpio.conf /etc/
 mkinitcpio -p linux
