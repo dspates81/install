5dbbc83a1269d0a20cfde2141a3a2e551ac076a6
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-07 18:47:33 -0400

    Update Arch_Installation.sh

--- Arch_Installation.sh
+++ Arch_Installation.sh
@@ -77,8 +77,8 @@ systemctl enable acpid
 
 pacman -S --needed grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-cp /ArchInst/grub /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
+cp /ArchInst/grub /etc/default/grub
 
 cp /ArchInst/mkinitcpio.conf /etc/
 mkinitcpio -p linux
