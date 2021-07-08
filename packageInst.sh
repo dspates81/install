917908ef9a7305c73d3dcff018bc68ceb4f5f944
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-08 16:30:27 -0400

    Update packageInst.sh

--- packageInst.sh
+++ packageInst.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-pacman -S --needed firefox dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pulseaudio pulseaudio-bluetooth pavucontrol pulseaudio-jack bash-completion openssh rsync reflector acpi acpi_call tlp edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld sof-firmware nss-mdns acpid os-prober ntfs-3g
+pacman -S --needed tlp firefox dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pulseaudio pulseaudio-bluetooth pavucontrol pulseaudio-jack bash-completion openssh rsync reflector acpi acpi_call tlp edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld sof-firmware nss-mdns acpid os-prober ntfs-3g
 
-systemctl enable NetworkManager
-systemctl enable bluetooth
-systemctl enable sshd
-systemctl enable avahi-daemon
-systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
-systemctl enable reflector.timer
-systemctl enable fstrim.timer
-systemctl enable libvirtd
-systemctl enable firewalld
-systemctl enable acpid
+sudo systemctl enable lightdm
+sudo systemctl enable sshd
+sudo systemctl enable avahi-daemon
+#sudo systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+#sudo systemctl enable bluetooth
+#sudo systemctl enable reflector.timer
+#sudo systemctl enable fstrim.timer
+#sudo systemctl enable libvirtd
+#sudo systemctl enable firewalld
+#sudo systemctl enable acpid
+
+
+#sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+#sudo firewall-cmd --add-port=1025-65535/udp --permanent
+#sudo firewall-cmd --reload
