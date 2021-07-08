1927435c38fbbdb083af7d8be87b4a8b4df56fab
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-08 15:34:54 -0400

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 
-sudo firewall-cmd --add-port=1025-65535/tcp --permanent
-sudo firewall-cmd --add-port=1025-65535/udp --permanent
-sudo firewall-cmd --reload
-
 
 git clone https://aur.archlinux.org/google-chrome.git
 mv /google-chrome
@@ -16,9 +12,8 @@ mv /google-chrome
 
 sudo pacman -S --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4-appfinder xfce4-panel xfce4-power-manager xfce4-session xfce4-settings xfconf xfdesktop xfwm4 xfwm4-themes arc-gtk-theme arc-icon-theme vlc xfburn xfce4-artwork xfce4-battery-plugin xfce4-clipman-plugin xfce4-cpugraph-plugin xfce4-datetime-plugin xfce4-mount-plugin xfce4-pulseaudio-plugin xfce4-screensaver xfce4-whiskermenu-plugin xfce4-weather-plugin xfce4-pulseaudio-plugin xfce4-taskmanager krusader hicolor-icon-theme kparts arj cpio kde-cli-tools kdiff3 keditbookmarks kompare konsole ktexteditor lhasa p7zip unace unrar unzip xz zip extra-cmake-modules kdoctools qbittorrent dolphin htop code gnome-disk-utility gedit terminator
 
+./systemctl.sh
 
-
-sudo systemctl enable lightdm
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
 sleep 5
 sudo reboot
