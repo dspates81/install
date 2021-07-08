eb227682729bcc13f78927a064c5524540e858e6
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-08 15:35:36 -0400

    Update systemctl.sh

--- systemctl.sh
+++ systemctl.sh
@@ -13,7 +13,6 @@ systemctl enable firewalld
 systemctl enable acpid
 
 
-sudo firewall-cmd --add-port=1025-65535/tcp --permanent
-sudo firewall-cmd --add-port=1025-65535/udp --permanent
-sudo firewall-cmd --reload
-
+#sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+#sudo firewall-cmd --add-port=1025-65535/udp --permanent
+#sudo firewall-cmd --reload
