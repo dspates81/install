56fff428cab08406b45b44d87885f837a62316a9
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-08 21:16:54 -0400

    Update UEFI.sh

--- UEFI.sh
+++ UEFI.sh
@@ -6,7 +6,6 @@ echo "-------------------------------------------------"
 echo "Setting up mirrors for optimal download - US Only"
 echo "-------------------------------------------------"
 
-pacman -S --noconfirm pacman-contrib curl
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 curl -s "https://archlinux.org/mirrorlist/?country=US&protocol=http&protocol=https&ip_version=4" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 
