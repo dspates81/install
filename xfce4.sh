8c633c9cf154a328dacc7b55794c4093a5f645e6
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-08 20:46:38 -0400

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -5,6 +5,8 @@
 #sudo firewall-cmd --add-port=1025-65535/udp --permanent
 #sudo firewall-cmd --reload
 
+mkdir Aur_file
+cd Aur_file
 
 git clone https://aur.archlinux.org/google-chrome.git
 cd google-chome/
@@ -20,6 +22,7 @@ cd ..
 #cd google-chome/
 #makepkg -isc --noconfirm/justin/google-chrome
 #cd ..
+cd ..
 
 #Xfce
 sudo pacman -S --needed exo garcon thunar thunar-volman tumbler xfce4-appfinder xfce4-panel xfce4-power-manager xfce4-session xfce4-settings xfconf xfdesktop xfwm4 xfwm4-themes thunar-archive-plugin thunar-media-tags-plugin xfburn xfce4-artwork xfce4-battery-plugin xfce4-clipman-plugin xfce4-cpufreq-plugin xfce4-cpugraph-plugin xfce4-datetime-plugin xfce4-diskperf-plugin xfce4-eyes-plugin xfce4-fsguard-plugin xfce4-genmon-plugin xfce4-mailwatch-plugin xfce4-mount-plugin xfce4-mpc-plugin xfce4-netload-plugin xfce4-notes-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screensaver xfce4-sensors-plugin xfce4-smartbookmark-plugin xfce4-systemload-plugin xfce4-taskmanager xfce4-time-out-plugin xfce4-timer-plugin xfce4-verve-plugin xfce4-wavelan-plugin xfce4-weather-plugin xfce4-xkb-plugin xfce4-whiskermenu-plugin pavucontrol pulseaudio pulseaudio-bluetooth pulseaudio-jack gedit lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings terminator xorg htop alsa-utils gnome-disk-utility hicolor-icon-theme sof-firmware qbittorrent
