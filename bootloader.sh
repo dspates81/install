c95ada22cba1d3d332b1436010cd279c77705456
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-08 21:14:53 -0400

    Update bootloader.sh

--- bootloader.sh
+++ bootloader.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-pacman -S --needed grub efibootmgr
+
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 cp /ArchInst/conf/grub /etc/default/grub
