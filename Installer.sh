a84eb3e6b10f9f354df1b13bcb44de222d58041b
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-08 20:42:48 -0400

    Update and rename Arch_Installation.sh to Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -8,14 +8,7 @@
 #   /_/ \_\_| \__|_||_|_|  |_\__,_|\__|_\__|
 #  Arch Linux Post Install Setup and Config
 #-------------------------------------------------------------------------
-pacmn -Sy
 
-echo "-------------------------------------------------"
-echo "Setting up mirrors for optimal download - US Only"
-echo "-------------------------------------------------"
-pacman -S --noconfirm pacman-contrib curl
-mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-curl -s "https://archlinux.org/mirrorlist/?country=US&protocol=http&protocol=https&ip_version=4" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 
 nc=$(grep -c ^processor /proc/cpuinfo)
 echo "You have " $nc" cores."
@@ -31,7 +24,7 @@ echo "-------------------------------------------------"
 
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 hwclock --systohc
-cp /ArchInst/conf/locale.gen /etc/locale.gen
+cp /ArchInst/locale.gen /etc/locale.gen
 locale-gen
 echo "Dspate81" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
@@ -39,9 +32,7 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 Dspates81.localdomain Dspates81" >> /etc/hosts
 
 
-echo "--------------------------------------"
-echo "-- Arch Install on Main Drive       --"
-echo "--------------------------------------"
+
 
 dd if=/dev/zero of=swapfile bs=1M count=1024 status=progress
 chmod 600 /swapfile
@@ -59,8 +50,6 @@ passwd
 useradd -mG wheel justin
 passwd justin
 
-pacman -S xdg-user-dirs xdg-utils networkmanager network-manager-applet 
+pacman -S networkmanager network-manager-applet 
 systemctl enable NetworkManager
-
-
 
