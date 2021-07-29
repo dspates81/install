980a7286f3cc3f093b17443993acd1e78058e0cb
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-29 13:09:18 -0400

    Add files via upload

---
+++ Aur.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir Aur_file
+
+https://aur.archlinux.org/brave.git
+https://aur.archlinux.org/vmware-workstation.git
+https://aur.archlinux.org/vmware-keymaps.git
+https://aur.archlinux.org/tiny-media-manager.git
