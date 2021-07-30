715607d2525a226045822a8c4a90614f346b1a55
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-29 23:40:33 -0400

    Update bootloader.sh

--- bootloader.sh
+++ bootloader.sh
@@ -3,8 +3,9 @@
 
 pacman -S --needed grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
 cp /install/conf/grub /etc/default/grub
+
+grub-mkconfig -o /boot/grub/grub.cfg
 
 cp /install/conf/mkinitcpio.conf /etc/
 mkinitcpio -p linux
