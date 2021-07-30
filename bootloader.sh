1fe05027c12683de2ea82cc5fc995c041879b880
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-29 20:57:22 -0400

    Update bootloader.sh

--- bootloader.sh
+++ bootloader.sh
@@ -3,15 +3,9 @@
 
 pacman -S --needed grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
 cp /ArchInst/conf/grub /etc/default/grub
+
+grub-mkconfig -o /boot/grub/grub.cfg
 
 cp /ArchInst/conf/mkinitcpio.conf /etc/
 mkinitcpio -p linux
-
-
-#bootctl --path=/boot install
-#cp loader.conf /boot/loader
-#cp arch.conf /boot/loader/entries
-
-
