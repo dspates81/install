468323eecaa663949fcbc4b9c9449ba995b76dd8
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-29 20:49:33 -0400

    Update uefi.sh

--- uefi.sh
+++ uefi.sh
@@ -36,14 +36,3 @@ echo "--------------------------------------"
 echo "--------------------------------------"
 
 systemctl enable NetworkManager
-systemctl enable bluetooth
-systemctl enable sshd
-systemctl enable avahi-daemon
-systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
-systemctl enable reflector.timer
-systemctl enable fstrim.timer
-systemctl enable libvirtd
-systemctl enable firewalld
-systemctl enable acpid
-systemctl enable smb.service
-
