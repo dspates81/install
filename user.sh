bea293294a878385acb0d66a5263ea8fcff0f825
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-08-07 20:27:21 -0400

    Update user.sh

--- user.sh
+++ user.sh
@@ -4,13 +4,8 @@ if ! source install.conf; then
 
 	read -sp "Please enter password:" password
 
-	read -sp "Please repeat password:" password2
-
-	# Check both passwords match
-	if [ "$password" != "$password2" ]; then
-	    echo "Passwords do not match"
-	    exit 1
-	fi
-    printf "username="$username"\n" >> "install.conf"
-  printf "password="$password"\n" >> "install.conf"
+	
+"
 fi
+
+useradd -mG wheel 
