8f8d416885002a1f17b903a71a44e42e5adccaba
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-08-13 19:56:12 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -4,7 +4,7 @@ pacman -S --noconfirm pacman-contrib curl
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 curl -s "https://archlinux.org/mirrorlist/?country=US&protocol=http&protocol=https&ip_version=4" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 
-pacman -S --needed  networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils bash-completion openssh rsync reflector acpi acpi_call tlp edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld sof-firmware nss-mdns acpid os-prober ntfs-3g  net-tools
+pacman -S --needed  networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers xdg-utils dnsutils alsa-utils bash-completion openssh rsync openbsd-netcat os-prober net-tools
 
-
+#xdg-user-dirs bluez bluez-utils  gvfs gvfs-smb nfs-utils inetutils ntfs-3g reflector acpi acpi_call tlp edk2-ovmf bridge-utils firewalld sof-firmware nss-mdns acpid avahi iptables-nft ipset dnsmasq vde2 
 systemctl enable NetworkManager
