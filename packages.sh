2db200c3f2224f1ceabfbfca4707cfc5c4695ea5
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-08-14 22:32:04 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -4,7 +4,7 @@ pacman -S --noconfirm pacman-contrib curl
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 curl -s "https://archlinux.org/mirrorlist/?country=US&protocol=http&protocol=https&ip_version=4" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 
-pacman -S --needed  networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector iproute2 dhcp dhclient dhcpcd dhcping wpa_supplicant wireless_tools netctl
+pacman -S --needed  networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector iproute2 dhcp dhclient dhcpcd dhcping wpa_supplicant wireless_tools netctl nfs-utils networkmanager-vpnc nss iputils netcf innernet iputils
  
 
 
