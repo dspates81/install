29012917e88186cc0677a8f946f2257a867ef5d7
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-08-15 12:54:05 -0400

    Create bootloader.sh

--- bootloader.sh
+++ bootloader.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
 
-pacman -S --needed grub efibootmgr
+pacman -S --needed grub efibootmgr dosfstools os-prober mtools
+
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+
 grub-mkconfig -o /boot/grub/grub.cfg
+
 cp /install/conf/grub /etc/default/grub
 
 cp /install/conf/mkinitcpio.conf /etc/
+
 mkinitcpio -p linux
-
-
-exit 
-sleep 5
-
-umount -l /mnt
-sleep 5
-
-reboot
